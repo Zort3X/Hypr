ce8c2665a2e3b40e6c193197d439599968076229
Z3R7 <marty.kamenow@gmail.com>
2024-12-15 01:01:37 +0200

    chainges to install.sh and removel of gtk setting because they are setted manually thrue the scrypt

--- install.sh
+++ install.sh
@@ -10,45 +10,36 @@ PARU_PACKAGES=(
 
 
 # === Step 1: Update pacman.conf ===
-echo "Updating pacman.conf..."
 cd ~
 sudo cp -f ~/Hypr/pacman.conf /etc/
 sudo cp -f ~/Hypr/nanorc /etc/
 
 # === Step 2: Install paru (AUR helper) ===
-echo "Installing paru..."
 git clone https://aur.archlinux.org/paru.git
 cd paru/
 makepkg -si --noconfirm
 cd ~
 sudo rm -rf paru/
 
-
 # === Step 3: Install essential packages with pacman ===
-echo "Installing essential packages with pacman..."
 sudo pacman -S --noconfirm "${PACMAN_PACKAGES[@]}"
 
 # === Step 4: Install essential packages with paru ===
-echo "Installing essential packages with paru..."
 paru -S --noconfirm "${PARU_PACKAGES[@]}"
 
-# === Step 5: Install oh-my-bash ===
-echo "Installing and setuping oh-my-bash..."
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-sudo cp -f ~/Hypr/.bashrc ~
-
-# === Step 6: Enable and disable systemd services ===
-echo "Configuring systemd services..."
+# === Step 5: Enable and disable systemd services ===
 sudo systemctl enable pipewire.service
 sudo systemctl enable ly.service
 sudo systemctl disable getty@tty2.service
 
-# === Step 7: Clone and copy dotfiles ===
+# === Step 6: Clone and copy dotfiles ===
 sudo cp -r ~/Hypr/.config ~
+gsettings set org.gnome.desktop.interface gtk-theme 'Gruvbox-Green-Dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Gruvbox-Dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Classic'
+gsettings set org.gnome.desktop.interface font-name 'SourceCodePro 11'
 
-# === Step 8: Final cleanup ===
-echo "Cleaning up unnecessary files..."
+# === Step 7: Final cleanup ===
 sudo pacman -Rns $(pacman -Qdtq) --noconfirm
 sudo pacman -Scc --noconfirm
-echo "Reboot your system!"
-sudo rm -rf ~/Hypr/
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" & sudo cp -f ~/Hypr/.bashrc ~ & echo "Reboot your system!" & sudo rm -rf ~/Hypr/
